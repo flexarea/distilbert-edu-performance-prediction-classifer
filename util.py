e3d60c7e2177420f3e0a4d040763161dff1ad2a4
esdras <esdrasntuyenabo40@gmail.com>
2024-07-31 12:28:53 +0200

    preparing dataset

--- util.py
+++ util.py
@@ -1,5 +1,9 @@
 from datasets import load_dataset
+from transformers import AutoTokenizer
 
 dataset = load_dataset("mstz/student_performance", "math")["train"]
+tokenizer = AutoTokenizer.from_pretrained("distilbert-base-uncased")
 
-print(dataset["reading_score"][:50])
+
+def tokenizer_function(data):
+    return tokenizer(data["text"], padding="max-length", truncation=True)
