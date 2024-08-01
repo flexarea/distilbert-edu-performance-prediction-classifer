cec1fa37ff59043954cf50731faea823a40987e4
esdras <esdrasntuyenabo40@gmail.com>
2024-08-01 11:58:55 +0200

    processing data

--- util.py
+++ util.py
@@ -2,6 +2,11 @@ from datasets import load_dataset
 from transformers import AutoTokenizer
 
 dataset = load_dataset("mstz/student_performance", "math")["train"]
+dataset = dataset.remove_columns(
+    ['is_male', 'ethnicity', 'parental_level_of_education', 'has_standard_lunch'])
+print(dataset)
+
+
 tokenizer = AutoTokenizer.from_pretrained("distilbert-base-uncased")
 
 
