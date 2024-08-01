ea0d327dca99171f0f8f55dea79a75a441938a2b
esdras <esdrasntuyenabo40@gmail.com>
2024-08-01 12:51:37 +0200

    p

--- util.py
+++ util.py
@@ -4,11 +4,14 @@ from transformers import AutoTokenizer
 dataset = load_dataset("mstz/student_performance", "math")["train"]
 dataset = dataset.remove_columns(
     ['is_male', 'ethnicity', 'parental_level_of_education', 'has_standard_lunch'])
-print(dataset)
 
-
+# initialize tokeniser
 tokenizer = AutoTokenizer.from_pretrained("distilbert-base-uncased")
 
+# extract data from a compile
+text_data = [str(i) for i in dataset["reading_score"][:15]]
 
-def tokenizer_function(data):
-    return tokenizer(data["text"], padding="max-length", truncation=True)
+# Tokeniser and create attention mask
+encoding = tokenizer(text_data, padding=True,
+                     truncation=True, return_tensors='pt')
+print(encoding)
